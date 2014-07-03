7657e3438d9e158150b0b610a0d211a5178afdd1
a272121742 <a272121742@gmail.com>
2014-07-03 16:56:49 +0800

    update
    修改文件名和项目名

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
-  summary:'Application Component 基础架构'
+  summary:'Appliance Component 基础架构'
 });
 
 Package.on_use(function(api){
   api.use('ui','client');
   api.use('reactive-dict',['client','server']);
-  api.add_files('Application.js','client');
+  api.add_files('Appliance.js','client');
 });
